3b691553dcfbdd70576e6b00798be47b67cd7dff
SivaRamaRao <sivaramarao2000@gmail.com>
2025-03-24 18:43:02 +0530

    Added !

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   
   return (
-   <div className='text-3xl font-bold text-green-800'>Hello React</div>
+   <div className='text-3xl font-bold text-green-800'>Hello React!</div>
   )
 }
 
